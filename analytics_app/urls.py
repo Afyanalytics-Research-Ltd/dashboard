f155d9266a2b7579255f7bd1024e03022956b9e5
lunyamwis <lutherlunyamwi@gmail.com>
2026-04-29 12:04:18 +0300

    adding ssl changes

--- analytics_app/urls.py
+++ analytics_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.dashboard_main, name = "dashboard_home"),
+    # path("", views.dashboard_main, name = "dashboard_home"),
+    path("", views.dashboard_list, name = "dashboard_home"),
     path("dashboard/list/", views.dashboard_list, name="dashboard_list"),
     path("dashboards/<slug:slug>/", views.dashboard_view, name="dashboard_view"),
 ]

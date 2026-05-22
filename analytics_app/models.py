4c3a5ea8711d7ab4531d7f4c3c47ab226b11567e
lunyamwis <lutherlunyamwi@gmail.com>
2026-05-22 17:17:27 +0300

    testing

--- analytics_app/models.py
+++ analytics_app/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 class Dashboard(models.Model):
-    name = models.CharField(max_length=1024)
+    name = models.CharField(max_length=2048)
     is_active = models.BooleanField(default=False)
     client = models.CharField(max_length=1024, null=True, blank=True)
     # THIS is the key part
